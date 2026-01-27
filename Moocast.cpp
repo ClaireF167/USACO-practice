03d8edabf6ed1951c7dbf23893285d5022e0c1bb
ClaireF167 <140350021+ClaireF167@users.noreply.github.com>
2026-01-26 20:20:08 -0800

    Add files via upload

---
+++ Moocast.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Cow{
+    int x, y, p;
+};
+
+bool visited[2000];
+vector<vector<int>> adj(2000);
+int dfs(int v){
+    visited[v]=true;
+    int count = 1;
+    for(int w : adj[v]){
+        if (!visited[w]){
+            count+=dfs(w);
+        }
+    }
+    return count;
+}
+
+int main() {
+	int n, x, y, p;
+    cin >> n;
+    vector<Cow> temp(n);
+    for(int i = 0; i < n; i++){
+        cin >> temp[i].x >> temp[i].y >> temp[i].p;
+    }
+
+//adjacency list
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if (i==j) continue;
+            if ((temp[i].x-temp[j].x)*(temp[i].x-temp[j].x)+(temp[i].y-temp[j].y)*(temp[i].y-temp[j].y)<temp[i].p*temp[i].p){
+                adj[i].push_back(j);
+            }
+        }
+    }
+
+//???
+    int maxcount = 1;
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            visited[j]=false;
+        }
+        int count = dfs(i);
+        maxcount = max(maxcount, count);
+    }
+
+    cout << maxcount << endl;
+}
